d49522d9ae3f7274b5d4fb58562d2d2c7c9a687d
ammar <ammarnazmi44@gmail.com>
2023-12-14 13:42:13 +0800

    feature

---
+++ src/components/users/EventInformation.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-col cols="8" offset="2" style="background-color: #b4c8bf;">
+    <div class="card">
+      <div class="card-header">
+        <h3>Video</h3>
+      </div>
+      <div class="card-body">
+        <h4>Race Pack 1</h4>
+        <p>Hello</p>
+      </div>
+    </div>
+  </v-col>
+</template>
+
+<Style lang="scss" scoped>
+section {
+  .card {
+    background-color: white;
+    width: 60%;
+  }
+
+  .card-header {
+    background-color: #4fa5bf;
+  }
+}
+</Style>
